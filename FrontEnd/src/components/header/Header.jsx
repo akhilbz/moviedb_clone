ad8a877b44053e4362353143dd60fdb1a9122737
Akhilesh Bitla <33524593+akhilbz@users.noreply.github.com>
2023-08-06 01:10:24 -0500

    Uploaded FrontEnd files

---
+++ FrontEnd/src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FaPlay } from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant='dark' expand='lg'>
+        <Container fluid>
+            <Navbar.Brand href='/' style={{color: 'gold'}}>
+                <FaPlay style={{height: '20px', width: '20px'}}/> 
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='navbarScroll' />
+            <Navbar.Collapse id='navbarScroll'>
+                <Nav 
+                className='me-auto my-2 my-lg-0'
+                style={{maxHeight: '100px'}}
+                navbarScroll>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/">Watch List</NavLink>
+                </Nav> 
+                <Button variant="outline-info" className="me-2">Login</Button>
+                <Button variant="outline-info">Register</Button>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
---
+++ FrontEnd/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../hero/Hero'
+
+const home = ({movies}) => {
+  return (
+    <Hero movies={movies}/>
+  )
+}
+
+export default home
